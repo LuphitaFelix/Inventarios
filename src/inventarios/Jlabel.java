63ff8c3ab97d6f5637d971fe43a2e384bf44f9f7
LuphitaFelix <lupita.2295@hotmail.com>
2016-02-24 18:54:17 -0600

    Interfaz graficade inicio

---
+++ src/inventarios/Jlabel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventarios;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author irene
+ */
+class Jlabel {
+
+    void setIcon(ImageIcon uno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setBounds(int i, double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
